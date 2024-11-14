6b8191af40a0519a76972fc65aeb06286c48ca3f
Jon Wang <jonwang.kuralabs@gmail.com>
2024-11-14 06:04:33 +0000

    adding install script for docker and terraform

---
+++ Scripts/install_docker_terraform.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Installing packages for Terraform
+sudo apt-get update
+sudo apt-get install -y gnupg software-properties-common
+
+# Installing Hashicorp GPG key
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+# Verifying Terraform key's fingerprint
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+
+# Should look something like this.
+# /usr/share/keyrings/hashicorp-archive-keyring.gpg
+# -------------------------------------------------
+# pub   rsa4096 XXXX-XX-XX [SC]
+# AAAA AAAA AAAA AAAA
+# uid           [ unknown] HashiCorp Security (HashiCorp Package Signing) <security+packaging@hashicorp.com>
+# sub   rsa4096 XXXX-XX-XX [E]
+
+# Adding Hashicorp repo to system.
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# Download and install terraform
+sudo apt-get update
+sudo apt-get install -y terraform
+
+# Add Docker's official gpg key
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install Docker Packages
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Post Install Docker Group
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
