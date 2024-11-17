0a7f33c8175fdcd9beb3172e3dd6ce981ad24424
Jon Wang <jonwang.kuralabs@gmail.com>
2024-11-17 03:29:26 +0000

    creating deploy.sh

--- Terraform/scripts/deploy.sh
+++ Terraform/scripts/deploy.sh
@@ -1,6 +1,57 @@
 #!/bin/bash
 
-# Installing Docker
+#############################
+### ADD CODON SSH PUB KEY ###
+#############################
+SSH_PUB_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDSkMc19m28614Rb3sGEXQUN+hk4xGiufU9NYbVXWGVrF1bq6dEnAD/VtwM6kDc8DnmYD7GJQVvXlDzvlWxdpBaJEzKziJ+PPzNVMPgPhd01cBWPv82+/Wu6MNKWZmi74TpgV3kktvfBecMl+jpSUMnwApdA8Tgy8eB0qELElFBu6cRz+f6Bo06GURXP6eAUbxjteaq3Jy8mV25AMnIrNziSyQ7JOUJ/CEvvOYkLFMWCF6eas8bCQ5SpF6wHoYo/iavMP4ChZaXF754OJ5jEIwhuMetBFXfnHmwkrEIInaF3APIBBCQWL5RC4sJA36yljZCGtzOi5Y2jq81GbnBXN3Dsjvo5h9ZblG4uWfEzA2Uyn0OQNDcrecH3liIpowtGAoq8NUQf89gGwuOvRzzILkeXQ8DKHtWBee5Oi/z7j9DGfv7hTjDBQkh28LbSu9RdtPRwcCweHwTLp4X3CYLwqsxrIP8tlGmrVoZZDhMfyy/bGslZp5Bod2wnOMlvGktkHs="
+
+echo "$SSH_PUB_KEY" >> /home/ubuntu/.ssh/authorized_keys
+
+#############################
+### NODE EXPORTER ###
+#############################
+# Install wget if not already installed
+sudo apt install wget -y
+
+# Download and install Node Exporter
+NODE_EXPORTER_VERSION="1.5.0"
+wget https://github.com/prometheus/node_exporter/releases/download/v$NODE_EXPORTER_VERSION/node_exporter-$NODE_EXPORTER_VERSION.linux-amd64.tar.gz
+tar xvfz node_exporter-$NODE_EXPORTER_VERSION.linux-amd64.tar.gz
+sudo mv node_exporter-$NODE_EXPORTER_VERSION.linux-amd64/node_exporter /usr/local/bin/
+rm -rf node_exporter-$NODE_EXPORTER_VERSION.linux-amd64*
+
+# Create a Node Exporter user
+sudo useradd --no-create-home --shell /bin/false node_exporter
+
+# Create a Node Exporter service file
+cat << EOF | sudo tee /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload systemd, start and enable Node Exporter service
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
+sudo systemctl enable node_exporter
+
+# Print the public IP address and Node Exporter port
+echo "Node Exporter installation complete. It's accessible at http://$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4):9100/metrics"
+
+
+#############################
+### DOCKER ###
+#############################
 ### Add Docker's official gpg key
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl
